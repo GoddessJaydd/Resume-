4fac7422eff77b4a18189355033ff5b034d06c96
Jonelle Alleyne <r.jonellealleyne@gmail.com>
2019-11-25 22:37:46 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+  function openNav(){
+        document.getElementById("hamburger").style.display="block";
+      }
+       function closeNav(){
+        document.getElementById("hamburger").style.display="none";
+      }
